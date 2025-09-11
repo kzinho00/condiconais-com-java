432be49c23f874c269e2450990086e3d6c846266
kzinho00 <kaua.campos.fernandes@escola.pr.gov.br>
2025-09-11 09:19:37 -0300

    Add files via upload

---
+++ atividades-de-java-condicionais/5b.js
@@ -0,0 +1,28 @@
+let dia = 10
+
+switch (dia) {
+    case 1:
+        console.log("domingo");
+        break;
+    case 2:
+        console.log("segunda");
+        break;
+    case 3:
+        console.log("terça");
+        break;
+    case 4:
+        console.log("quarta");
+        break;
+    case 5:
+        console.log("quinta");
+        break;
+    case 6:
+        console.log("sexta");
+        break;
+    case 7:
+        console.log("sábado");
+        break;
+    default:
+        console.log("dia inválido");
+        break;
+}
